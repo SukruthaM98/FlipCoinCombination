b107d8e8b040ff76e21ef4e68920559ea2fd2a67
SukruthaM98 <msukrutha@gmail.com>
2020-10-09 08:40:54 +0530

    percentage of singlet combination evaluated

--- FlipcoinCombination.sh
+++ FlipcoinCombination.sh
@@ -2,11 +2,31 @@
 
 echo "welcome to flip coin combination"
 
-coin=$((RANDOM%2))
+declare -A coin
+coin[heads]=0
+coin[tails]=0
 
-if [ $coin -eq 0 ]
+for (( i=0; i<20; i++ ))
+do
+coinflip=$((RANDOM%2))
+
+if [ $coinflip -eq 0 ]
 then
 	echo " coin flips head "
+	coin[heads]=$(( ${coin[heads]}+1 ))
 else
 	echo "coin flips tail "
+	coin[tails]=$(( ${coin[tails]}+1 ))
 fi
+
+done
+
+echo "number of times head flips:" ${coin[heads]} 
+echo "number of times tail flips:" ${coin[tails]}
+
+sumofheadtail=$(( ${coin[heads]}+${coin[tails]} ))
+percentageofHeads=$(( (${coin[heads]}*100)/$sumofheadtail ))
+percentageofTails=$(( (${coin[tails]}*100)/$sumofheadtail ))
+
+echo "percentage of heads:" $percentageofHeads"%"
+echo "percentage of tails:" $percentageofTails"%"
